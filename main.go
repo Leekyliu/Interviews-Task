87ba81705a980eb527b07821b971ae80b9f281b7
Leeky <leekylu17@gmail.com>
2019-08-12 16:18:23 +1000

    1.1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 
 	"github.com/veandco/go-sdl2/sdl"
@@ -15,17 +16,26 @@ const (
 )
 
 func main() {
-	run()
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
 }
 
 func run() error {
-
-
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
 
 	window, err := sdl.CreateWindow(
 		windowTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		windowWidth, windowHeight, sdl.WINDOW_SHOWN,
 	)
+	if err != nil {
+		return fmt.Errorf("could not create window: %v", err)
+	}
+	defer window.Destroy()
 
 	scene, err := newScene(window)
 	if err != nil {
@@ -44,5 +54,5 @@ func run() error {
 		}
 	}
 
-
+	
 }
